fe34e38e308d77afa7773cf6d434295134ffc119
illujun <tkdwnsdkdlel@gmail.com>
2025-10-30 02:44:19 +0900

    2nd book til word2vec

--- common/layers.py
+++ common/layers.py
@@ -282,3 +282,22 @@ class Pooling:
         dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
         
         return dx
+
+class MatMul:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.x = None
+        
+    def forward(self, x):
+        W, = self.params
+        out = np.matmul(x, W)
+        self.x = x
+        return out
+    
+    def backward(self, dout):
+        W, = self.params
+        dx = np.matmul(dout, W.T)
+        dW = np.matmul(self.x.T, dout)
+        self.grads[0][...] = dW  # 깊은 복사
+        return dx
